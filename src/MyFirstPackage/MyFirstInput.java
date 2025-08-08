86402b9fd37fb15979a93e46424a5789808268d2
rositariv <rosinka666@gmail.com>
2025-08-09 01:11:40 +0300

    Lecture1 commit

---
+++ src/MyFirstPackage/MyFirstInput.java
@@ -0,0 +1,30 @@
+package MyFirstPackage;
+
+import java.util.Scanner;
+
+public class MyFirstInput {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("-----------------");
+        System.out.println("The entered name is: " + name);
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.println("-----------------");
+        System.out.println("The age of " + name + " is: " + age);
+        System.out.println("Enter your height:");
+        double height = scanner.nextDouble();
+        System.out.println("Your height is: " + height);
+
+        System.out.println("Please enter two numbers: ");
+        int numberOne = scanner.nextInt();
+        int numberTwo = scanner.nextInt();
+        System.out.println("No brackets");
+        System.out.println("The sum of numbers is: " + numberTwo+numberOne);
+        System.out.println("With brackets");
+        System.out.println("The sum of numbers is: " + (numberTwo+numberOne));
+    }
+}
--- src/MyFirstPackage/Operators.java
+++ src/MyFirstPackage/Operators.java
@@ -2,5 +2,21 @@ package MyFirstPackage;
 
 public class Operators {
 
+    public static void main(String[] args){
+        int numberOne = 100;
+        int numberTwo = 30;
+
+        System.out.println(numberOne + numberTwo);
+        System.out.println(numberOne - numberTwo);
+        System.out.println(numberOne * numberTwo);
+        System.out.println(numberOne / numberTwo);
+        System.out.println(numberOne % numberTwo);
+        System.out.println(++numberOne);
+        System.out.println(--numberTwo);
+
+        double numberDivision = (double) numberOne / numberTwo;
+        System.out.println(numberDivision);
+        System.out.println((double) numberOne / numberTwo);
+    }
     
 }
